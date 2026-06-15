16c82fefa3e88f68755372d9f80d7f6d0efdbc39
Hyunsoo <jing07161@gmail.com>
2026-06-15 17:04:17 +0900

    section2>8번 다시 풀기

--- section2/8.뒤집은소수.py
+++ section2/8.뒤집은소수.py
@@ -13,6 +13,51 @@ def reverse(x):
         x=x//10
     return res
 
+def isPrime(x):
+    if x==1:
+        return False
+    for i in range(2, (x//2)+1):
+        if x%i==0:
+            return False
+    else:
+        return True
+    
+for i in numbers:
+    res=reverse(i)
+    
+    if isPrime(res)==True:
+        print(res, end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def reverse(x):
+    res=0
+    while x>0:
+        t=x%10
+        res=res*10+t
+        x=x//10
+    return res
+
 # def reverse(x):
 #     tmp=list(str(x))
 #     tmp.reverse()
@@ -32,6 +77,6 @@ for i in numbers:
     res=reverse(i)
     if isPrime(res)==True:
         print(res, end=' ')
-    
+"""    
     
 
