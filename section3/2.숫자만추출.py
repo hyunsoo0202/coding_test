8830ac2f012984c77f1b0e4e39cf0f29ca634cb4
Hyunsoo <jing07161@gmail.com>
2026-06-15 17:48:10 +0900

    section3>2번 문제 다시 풀기

--- section3/2.숫자만추출.py
+++ section3/2.숫자만추출.py
@@ -3,6 +3,43 @@ sys.stdin=open('input.txt', 'rt')
 
 n=input()
 
+res=""
+for i in n:
+    if i.isdecimal()==True:
+        res+=i
+
+res=int(res)
+cnt=0
+for i in range(1,res+1):
+    if res%i==0:
+        cnt+=1
+
+print(res)
+print(cnt)
+
+
+
+    # res=""
+    # if str.isdecimal(n[i])==True:
+    #     res+=n[i]
+
+    # print(int(res))
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 res=0
 for i in n:
     if i.isdecimal():
@@ -16,4 +53,4 @@ for i in range(1, res+1):
         cnt+=1
 print(res)        
 print(cnt)
-    
+    """
