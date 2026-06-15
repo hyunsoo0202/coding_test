1727380e219a86d286752079dcb552afb5bde903
Hyunsoo <jing07161@gmail.com>
2026-06-15 16:42:33 +0900

    section2>7번 다시 풀기

--- section2/7.소수.py
+++ section2/7.소수.py
@@ -3,6 +3,39 @@ sys.stdin=open('input.txt', 'rt')
 n=int(input())
 
 
+print(n)
+cn=[0]*(n+1)
+cnt=0
+print(cn)
+
+for i in range(2, (n+1)):
+    if cn[i]==0:
+        cnt+=1
+        for j in range(i, (n+1), i):
+            cn[j]=1
+
+print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# 정답 코드
 ch=[0]*(n+1)
 cnt=0
 
@@ -14,14 +47,4 @@ for i in range(2,n+1):
 
 
 print(cnt)
-
-# cnt=0
-# for x in range(2,n+1):
-#     # print(x)
-#     for i in range(2, x):
-#         if x%i==0:
-#             break
-#     else:
-#         cnt+=1
-            
-# print(cnt)
+"""
