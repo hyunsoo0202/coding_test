734698f71b0eee441226c8dc581f3464a5c788e7
Hyunsoo <jing07161@gmail.com>
2026-06-15 17:31:40 +0900

    section3>1번 문제 다시 풀기

--- section3/1.회문문자열검사.py
+++ section3/1.회문문자열검사.py
@@ -5,6 +5,46 @@ n=int(input())
 print(n)
 
 for i in range(n):
+    str=input()
+    low_str=str.lower()
+    # print(low_str)
+
+    for j in range(0, (len(low_str)//2)+1):
+        if low_str[j]!=low_str[-j-1]:
+            print(f"#{i+1} NO")
+            break
+    else:
+        print(f"#{i+1} YES")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+for i in range(n):
     str=input().lower()
     # print(str[0])
     for x in range(len(str)//2):
@@ -13,3 +53,4 @@ for i in range(n):
             break
     else:
         print('#%d YES' %(i+1))
+"""
