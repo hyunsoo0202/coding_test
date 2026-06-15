886639c8d5c6e49288acdabc61d2bf9f7d5908d1
Hyunsoo <jing07161@gmail.com>
2026-06-15 18:11:32 +0900

    section3>3번 다시 풀기

--- section3/3.카드역배치.py
+++ section3/3.카드역배치.py
@@ -1,15 +1,40 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-number_list=list(range(1, 21))
-
-print(number_list)
+arr=list(range(1, 21))
 
 
+
+for _ in range(10):
+    [a,b]=map(int, input().split())
+    print(a,b)
+    for i in range((b-a+1)//2):
+        arr[a-1+i],arr[b-1-i]=arr[b-1-i],arr[a-1+i]
+print(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 for _ in range(10):
     a,b=map(int, input().split())
     for i in range((b-a+1)//2):
         number_list[a-1+i], number_list[b-1-i]=number_list[b-1-i], number_list[a-1+i]
 
 print(number_list)   
-    
+"""    
