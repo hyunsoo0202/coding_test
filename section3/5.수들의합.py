f160b0a2994571357e49d3b2f7382f4b3252b45d
Hyunsoo <jing07161@gmail.com>
2026-06-16 15:00:16 +0900

    section3>5번 다시 풀기

--- section3/5.수들의합.py
+++ section3/5.수들의합.py
@@ -1,6 +1,76 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
+[n,m]=map(int,input().split())
+arr=list(map(int, input().split()))
+
+lt=0
+rt=0
+cnt=0
+tot=0
+
+while True:
+    if tot<m:
+        if rt<n:
+            tot+=arr[rt]
+            rt+=1
+        else:
+            break
+    elif tot==m:
+        cnt+=1
+        tot-=arr[lt]
+        lt+=1
+    elif tot>m:
+        tot-=arr[lt]
+        lt+=1
+
+print(cnt)
+
+    
+
+# while r<n:
+#     sum=0
+#     for i in range(l, r+1):
+#         sum+=arr[i]
+    
+#     if sum<m:
+#         r+=1
+#     elif sum==m:
+#         cnt+=1
+#         l+=1
+#     else:
+#         l+=1
+# print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 n,m=map(int,input().split())
 print(n,m)
 arr=list(map(int,input().split()))
@@ -27,17 +97,4 @@ while True:
         tot-=arr[lt]
         lt+=1
 print(cnt)
-
-
-# cnt=0
-# for i in range(n):
-#     for j in range(i, n):
-        
-#         res=sum(arr[i:j+1])
-#         if res==m:
-#             cnt+=1
-#             break
-#         elif res>m:
-#             break
-# print(cnt)
-        
+"""
