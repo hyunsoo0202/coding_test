acd39d1827a66633edd70bc338f1fd7e6990d176
Hyunsoo <jing07161@gmail.com>
2026-06-16 14:22:55 +0900

    section3>4번 다시 풀기

--- section3/4.두리스트합치기.py
+++ section3/4.두리스트합치기.py
@@ -6,6 +6,53 @@ a=list(map(int, input().split()))
 m=int(input())
 b=list(map(int, input().split()))
 
+print(a)
+print(b)
+
+p1=0
+p2=0
+res=[]
+# print(i,j)
+
+for i in range(n+m):
+    if a[p1]<=b[p2]:
+        res.append(a[p1])
+        p1+=1
+    else:
+        res.append(b[p2])
+        p2+=1
+    
+    if p1==n:
+        res.extend(b[p2:])
+        break
+    if p2==m:
+        res.extend(a[p1:])
+        break
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+n=int(input())
+a=list(map(int, input().split()))
+m=int(input())
+b=list(map(int, input().split()))
+
 p1=0
 p2=0
 c=[]
@@ -22,6 +69,6 @@ if p1<n:
 if p2<m:
     c=c+b[p2:]
 print(c)
-
+"""
 
     
