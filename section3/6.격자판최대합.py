31f01fdcccde40c8f72c3bda91d9110028fdf488
Hyunsoo <jing07161@gmail.com>
2026-06-18 19:34:37 +0900

    section3>6번 다시 풀기

--- section3/6.격자판최대합.py
+++ section3/6.격자판최대합.py
@@ -1,6 +1,81 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
+
+n=int(input())
+arr=[list(map(int, input().split())) for _ in range(n)]
+
+print(arr)
+max_val=-2147000000
+
+for i in range(n):
+    sum1=0
+    sum2=0
+    for j in range(n):
+        sum1+=arr[i][j]
+        sum2+=arr[j][i]
+    if max_val<sum1:
+        max_val=sum1
+    if max_val<sum2:
+        max_val=sum2
+
+sum3=0
+sum4=0
+for i in range(n):
+    sum3+=arr[i][i]
+    sum4+=arr[i][n-1-i]
+    
+if max_val<sum3:
+    max_val=sum3
+
+print(max_val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 max=-2147000000
 
 n=int(input())
@@ -33,3 +108,4 @@ if sum2>max:
     max=sum2
 
 print(max)
+"""
