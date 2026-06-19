fbea9aced5843812c9bccddda26a18ff4bbe8b68
Hyunsoo <jing07161@gmail.com>
2026-06-19 19:33:31 +0900

    section3>9번 다시 풀기

--- section3/9.봉우리.py
+++ section3/9.봉우리.py
@@ -2,6 +2,49 @@ import sys
 sys.stdin=open('input.txt', 'rt')
 
 n=int(input())
+arr=[list(map(int, input().split())) for i in range(n)]
+direct=[[-1,0],[1,0],[0,-1],[0,1]]
+cnt=0
+for i in range(n):
+    for j in range(n):
+
+        for [dx, dy] in direct:
+            x=i+dx  
+            y=j+dy
+
+            if x<0 or x>n-1 or y<0 or y>n-1:
+                continue
+            if arr[i][j]<arr[x][y]:
+                break
+        else:
+            cnt+=1
+
+print(cnt)
+
+# for [dx,dy] in direct:
+#     print(dx, dy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+n=int(input())
 arr=[list(map(int, input().split())) for _ in range(n)]
 
 print(arr)
@@ -39,3 +82,4 @@ for i in range(n):
         #i-1,j #좌
         #i+1,j #우
 print(cnt)
+"""
