8150907ca44a427f3e6e200f99808d9750d9db5a
Hyunsoo <jing07161@gmail.com>
2026-06-25 17:40:33 +0900

    section5>4번 풀기

---
+++ section5/4.후위식연산.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin=open('input.txt', 'rt')
+
+a=input()
+stack=[]
+res=0
+
+for x in a:
+  if x.isdecimal():
+    stack.append(int(x))
+  else: 
+    target1=0
+    target2=0
+    
+    target2=stack.pop()
+    target1=stack.pop()
+    
+    if x=='+':
+      stack.append(int(target1)+int(target2))
+    elif x=='-':
+      stack.append(int(target1)-int(target2))
+    elif x=='*':
+      stack.append(int(target1)*int(target2))
+    elif x=='/':
+      stack.append(int(target1)/int(target2))
+
+print(stack[0])
+
+"""
+- 숫자 -> stack에 append
+- 연산자
+  - stack.pop(): 두번째 피연산자
+  - stack.pop(): 첫번째 피연산자
+  - 계산 후 stack에 append
+- 반복문 끝나고 stack에 있는 값 출력
+"""
