6c55283a1c4407def39bf2fe89d1f35ceb5c3587
Hyunsoo <jing07161@gmail.com>
2026-07-08 11:47:28 +0900

    section4>2번 다시 풀기

--- section4/2.랜선자르기(결정알고리즘).py
+++ section4/2.랜선자르기(결정알고리즘).py
@@ -1,32 +1,90 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-k, n=map(int, input().split())
+k,n=map(int, input().split())
+print(k,n)
 line=[]
-res=0 # 최대값
 largest=0
+for x in range(k):
+  item=int(input())
+  line.append(item)
+  largest=max(item, largest)
+print(line)
+print(largest)
 
-for _ in range(k):
-    item=int(input())
-    line.append(item)
-    largest=max(largest, item)
-
-lt=0
+lt=1
 rt=largest
-
 def count(len):
-    cnt=0
-    for i in line:
-        cnt+=(i//len)
-    return cnt
-
+  cnt=0
+  for x in line:
+    cnt+=x//len
+  return cnt
+  
+res=0
 while lt<=rt:
-    mid=(lt+rt)//2
+  mid=(lt+rt)//2
+  if count(mid)<n:
+    rt=mid-1
+  else:
+    res=mid
+    lt=mid+1
 
-    if count(mid)<n:
-        rt=mid-1
-    else:
-        res=mid
-        lt=mid+1
+  
 
-print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# k, n=map(int, input().split())
+# line=[]
+# res=0 # 최대값
+# largest=0
+
+# for _ in range(k):
+#     item=int(input())
+#     line.append(item)
+#     largest=max(largest, item)
+
+# lt=0
+# rt=largest
+
+# def count(len):
+#     cnt=0
+#     for i in line:
+#         cnt+=(i//len)
+#     return cnt
+
+# while lt<=rt:
+#     mid=(lt+rt)//2
+
+#     if count(mid)<n:
+#         rt=mid-1
+#     else:
+#         res=mid
+#         lt=mid+1
+
+# print(res)
