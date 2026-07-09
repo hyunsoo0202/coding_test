8e8c3a16fb159633c7b023541b964e58cef1b2ec
Hyunsoo <jing07161@gmail.com>
2026-07-09 15:01:42 +0900

    section4>4번 다시 풀기

--- section4/4.마구간 정하기(결정알고리즘).py
+++ section4/4.마구간 정하기(결정알고리즘).py
@@ -2,38 +2,85 @@ import sys
 sys.stdin=open('input.txt', 'rt')
 
 n,c=map(int, input().split())
+arr=[int(input()) for _ in range(n)]
+arr.sort()
+print(arr)
 
-cord=[]
-for _ in range(n):
-    cord.append(int(input()))
-cord.sort()
-
-lt=1
-rt=max(cord)-min(cord)
+lt=1 # 마구간 사이 최소 거리
+rt=max(arr)-min(arr) # 마구간 사이 최대 거리
 res=0
 def check(len):
-    cnt=1
-    ep=cord[0]    
-    for i in range(1, n):
-        if cord[i]-ep>=len:
-            cnt+=1
-            ep=cord[i]
-    return cnt
+  cnt=1
+  ep=arr[0]
+  for i in range(1, n):
+    if arr[i]-ep>=len:
+      cnt+=1
+      ep=arr[i]
+  return cnt
 
 while lt<=rt:
-    mid=(lt+rt)//2
-    if check(mid)<c:
-        # 말들 사이의 거리가 너무 멀기 때문에 말을 다 배치 못한것
-        # 거리를 줄여야함
+  
+  mid=(lt+rt)//2
+  if check(mid)<c:
+    rt=mid-1
+  else:
+    res=mid
+    lt=mid+1
 
-        rt=mid-1
-    elif check(mid)>=c:
-        res=mid
-        lt=mid+1
-        # 말들 사이 거리가 너무 가깝기 때문에 말을 c 마리 이상 배치 가능한 것
-        # 거리를 늘려야함
-        # 혹은 c 마리보다 더 배치할 수 있는 말들 사이의 거리라면
-        # 이것 또한 정답이 될 수는 있음 
-        # -> 3마리를 배치해야 하는데 4마리를 배치할 수 있는 상황이라는건 3마리 배치도 가능한 거리이기 때문
-print(mid)
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n,c=map(int, input().split())
+
+# cord=[]
+# for _ in range(n):
+#     cord.append(int(input()))
+# cord.sort()
+
+# lt=1
+# rt=max(cord)-min(cord)
+# res=0
+# def check(len):
+#     cnt=1
+#     ep=cord[0]    
+#     for i in range(1, n):
+#         if cord[i]-ep>=len:
+#             cnt+=1
+#             ep=cord[i]
+#     return cnt
+
+# while lt<=rt:
+#     mid=(lt+rt)//2
+#     if check(mid)<c:
+#         # 말들 사이의 거리가 너무 멀기 때문에 말을 다 배치 못한것
+#         # 거리를 줄여야함
+
+#         rt=mid-1
+#     elif check(mid)>=c:
+#         res=mid
+#         lt=mid+1
+#         # 말들 사이 거리가 너무 가깝기 때문에 말을 c 마리 이상 배치 가능한 것
+#         # 거리를 늘려야함
+#         # 혹은 c 마리보다 더 배치할 수 있는 말들 사이의 거리라면
+#         # 이것 또한 정답이 될 수는 있음 
+#         # -> 3마리를 배치해야 하는데 4마리를 배치할 수 있는 상황이라는건 3마리 배치도 가능한 거리이기 때문
+# print(mid)
 
