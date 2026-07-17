ac26aa48f9a6bd76133e76f2c3090e1c7fc738ea
Hyunsoo <jing07161@gmail.com>
2026-07-17 13:11:10 +0900

    section4>6번 다시 풀기

--- section4/6.씨름선수(그리디).py
+++ section4/6.씨름선수(그리디).py
@@ -1,29 +1,59 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-n=int(input())
-arr=[]
-for _ in range(n):
-    a,b=map(int, input().split())
-    arr.append([a,b])
+n=int(input()) # 지원자 수
 
-arr.sort(reverse=True)
+arr=[list(map(int, input().split())) for _ in range(n)]
+
+arr.sort(key=lambda x:x[1], reverse=True)
+# arr.sort(reverse=True)
 print(arr)
 
 largest=0
 cnt=0
-for x in arr:
-    if x[1]>largest:
-        cnt+=1
-        largest=x[1]
-print(cnt)
-# cnt=0
-# for a,b in arr:
-#     print(a,b)
-#     for c,d in arr:
-#         if a<c and b<d:
-#             break
-#     else:
-#         cnt+=1
+for i in range(n):
+  if largest<arr[i][0]:
+    largest=arr[i][0]
+    cnt+=1
 
+print(cnt)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input())
+# arr=[]
+# for _ in range(n):
+#     a,b=map(int, input().split())
+#     arr.append([a,b])
+
+# arr.sort(reverse=True)
+# print(arr)
+
+# largest=0
+# cnt=0
+# for x in arr:
+#     if x[1]>largest:
+#         cnt+=1
+#         largest=x[1]
 # print(cnt)
