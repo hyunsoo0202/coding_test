3eeeb2655ddef7f50e9b2ab0e0ab3150bdacd1f3
Hyunsoo <jing07161@gmail.com>
2026-07-20 16:39:37 +0900

    section4>9번 다시 풀기

--- section4/9.증가수열만들기.py
+++ section4/9.증가수열만들기.py
@@ -1,32 +1,88 @@
 import sys
-from collections import deque
-
 sys.stdin=open('input.txt', 'rt')
+from collections import deque
 
 n=int(input())
 arr=list(map(int, input().split()))
-print(arr)
-ep=0
+
 lt=0
 rt=n-1
 tmp=[]
 res=""
+min_val=0
 
 while lt<=rt:
-    if ep<arr[lt]:
-        tmp.append((arr[lt], "L"))
-    if ep<arr[rt]:
-        tmp.append((arr[rt], "R"))
-    if len(tmp)==0:
-        break
+  if arr[lt]>min_val:
+    tmp.append((arr[lt], "L"))
+  if arr[rt]>min_val:
+    tmp.append((arr[rt], "R"))
+  if len(tmp)==0:
+    break
+  else:
+    tmp.sort()
+    res+=tmp[0][1]
+    min_val=tmp[0][0]
+    if tmp[0][1]=="L":
+      lt+=1
     else:
-        tmp.sort()
-        res+=tmp[0][1]
-        ep=tmp[0][0]
-        if tmp[0][1]=="L":
-            lt+=1
-        else:
-            rt-=1
-        tmp.clear()
+      rt-=1
+  tmp.clear()
+print(len(res))
 print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input())
+# arr=list(map(int, input().split()))
+# print(arr)
+# ep=0
+# lt=0
+# rt=n-1
+# tmp=[]
+# res=""
+
+# while lt<=rt:
+#     if ep<arr[lt]:
+#         tmp.append((arr[lt], "L"))
+#     if ep<arr[rt]:
+#         tmp.append((arr[rt], "R"))
+#     if len(tmp)==0:
+#         break
+#     else:
+#         tmp.sort()
+#         res+=tmp[0][1]
+#         ep=tmp[0][0]
+#         if tmp[0][1]=="L":
+#             lt+=1
+#         else:
+#             rt-=1
+#         tmp.clear()
+# print(res)
         
