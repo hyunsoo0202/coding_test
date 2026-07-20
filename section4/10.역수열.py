0da0e588e230ed77c60ab841a3e3e986736ea69f
Hyunsoo <jing07161@gmail.com>
2026-07-20 17:10:52 +0900

    section4>10번 다시 풀기

--- section4/10.역수열.py
+++ section4/10.역수열.py
@@ -3,24 +3,65 @@ sys.stdin=open('input.txt', 'rt')
 
 n=int(input())
 arr=list(map(int, input().split()))
-
+ch=[0]*n
 print(arr)
 
-tmp=[0]*n
 for i in range(n):
-    cnt=arr[i]
-    number=i+1
-    for j in range(n):
-        if cnt==0 and tmp[j]==0:
-            tmp[j]=number
-            break    
-            # if tmp[j]!=0:
-            #     continue
-            # else:
-            #     tmp[j]=number
-            #     break
-        elif tmp[j]==0:
-            cnt-=1
-            continue
+  cur_val=i+1
+  cnt=arr[i]
 
-print(tmp)
+  for j in range(n):
+    if cnt==0 and ch[j]==0:
+      ch[j]=cur_val
+      break
+    elif ch[j]==0:
+      cnt-=1
+for x in ch:
+  print(x, end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input())
+# arr=list(map(int, input().split()))
+
+# print(arr)
+
+# tmp=[0]*n
+# for i in range(n):
+#     cnt=arr[i]
+#     number=i+1
+#     for j in range(n):
+#         if cnt==0 and tmp[j]==0:
+#             tmp[j]=number
+#             break    
+#             # if tmp[j]!=0:
+#             #     continue
+#             # else:
+#             #     tmp[j]=number
+#             #     break
+#         elif tmp[j]==0:
+#             cnt-=1
+#             continue
+
+# print(tmp)
