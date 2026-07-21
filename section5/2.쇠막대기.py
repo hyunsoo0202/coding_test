aa5c0203bbef2f8dd24139112e7fedb1e1056d1d
Hyunsoo <jing07161@gmail.com>
2026-07-21 22:46:20 +0900

    section5>2번 다시 풀기

--- section5/2.쇠막대기.py
+++ section5/2.쇠막대기.py
@@ -1,21 +1,60 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-arr=input()
-print(arr)
-
-prev=''
-tot=0
+arr=list(input())
 stack=[]
+cnt=0
+prev=""
+for x in arr:
+  if x=="(":
+    stack.append(x)
+  else: 
+    if prev=="(":
+      stack.pop()
+      cnt+=len(stack)
+    elif prev==")":
+      stack.pop()
+      cnt+=1
+  prev=x
 
-for i in range(len(arr)):
-  if arr[i]=='(':
-    stack.append(arr[i])
-  else:
-    if arr[i-1]=='(':
-      stack.pop()
-      tot+=len(stack)
-    else:
-      stack.pop()
-      tot+=1
-print(tot)
+print(cnt)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arr=input()
+# print(arr)
+
+# prev=''
+# tot=0
+# stack=[]
+
+# for i in range(len(arr)):
+#   if arr[i]=='(':
+#     stack.append(arr[i])
+#   else:
+#     if arr[i-1]=='(':
+#       stack.pop()
+#       tot+=len(stack)
+#     else:
+#       stack.pop()
+#       tot+=1
+# print(tot)
