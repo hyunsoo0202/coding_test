4303b8e4039e940bf0341372089e4ab2073dda4e
Hyunsoo <jing07161@gmail.com>
2026-07-21 22:27:03 +0900

    section5>1번 다시 풀기

--- section5/1.가장큰수.py
+++ section5/1.가장큰수.py
@@ -1,24 +1,72 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-arr,cnt=map(int, input().split())
-
-print(type(arr))
-arr=list(map(int, str(arr)))
-print(arr)
-# print(n)
-
+n,m=map(int, input().split())
+arr=[int(x) for x in str(n)]
 stack=[]
+
 for x in arr:
-  while stack and cnt>0 and stack[-1]<x:
+  while m>0 and stack and stack[-1]<x:
     stack.pop()
-    cnt-=1
+    m-=1
   stack.append(x)
+  
+if m>0:
+  stack=stack[:-m]
+for x in stack:
+  print(x, end='')
+  
 
 
-if cnt!=0:
-  stack=stack[:-cnt]
-print("".join(map(str,stack)))      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arr,cnt=map(int, input().split())
+
+# print(type(arr))
+# arr=list(map(int, str(arr)))
+# print(arr)
+# # print(n)
+
+# stack=[]
+# for x in arr:
+#   while stack and cnt>0 and stack[-1]<x:
+#     stack.pop()
+#     cnt-=1
+#   stack.append(x)
+
+
+# if cnt!=0:
+#   stack=stack[:-cnt]
+# print("".join(map(str,stack)))      
       
     
 # for _ in range(n):
