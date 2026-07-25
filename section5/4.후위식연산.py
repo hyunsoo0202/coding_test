bcc2148011518c787b619a62af6c88a83c7bfb36
Hyunsoo <jing07161@gmail.com>
2026-07-25 18:03:54 +0900

    section5>4번 다시 풀기

--- section5/4.후위식연산.py
+++ section5/4.후위식연산.py
@@ -1,30 +1,80 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-a=input()
+arr=input()
+print(arr)
 stack=[]
-res=0
-
-for x in a:
+for x in arr:
   if x.isdecimal():
-    stack.append(int(x))
-  else: 
-    target1=0
-    target2=0
-    
-    target2=stack.pop()
-    target1=stack.pop()
-    
-    if x=='+':
-      stack.append(int(target1)+int(target2))
-    elif x=='-':
-      stack.append(int(target1)-int(target2))
-    elif x=='*':
-      stack.append(int(target1)*int(target2))
-    elif x=='/':
-      stack.append(int(target1)/int(target2))
+    stack.append(x)
+  else:
+    num1=int(stack.pop())
+    num2=int(stack.pop())
 
+    if x=="+":
+      stack.append(num2+num1)
+    elif x=="-":
+      stack.append(num2-num1)
+    elif x=="*":
+      stack.append(num2*num1)
+    elif x=="/":
+      stack.append(num2/num1)
 print(stack[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a=input()
+# stack=[]
+# res=0
+
+# for x in a:
+#   if x.isdecimal():
+#     stack.append(int(x))
+#   else: 
+#     target1=0
+#     target2=0
+    
+#     target2=stack.pop()
+#     target1=stack.pop()
+    
+#     if x=='+':
+#       stack.append(int(target1)+int(target2))
+#     elif x=='-':
+#       stack.append(int(target1)-int(target2))
+#     elif x=='*':
+#       stack.append(int(target1)*int(target2))
+#     elif x=='/':
+#       stack.append(int(target1)/int(target2))
+
+# print(stack[0])
 
 """
 - 숫자 -> stack에 append
