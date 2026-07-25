10c73955e0a30c0c770420681c4a3acae1648028
Hyunsoo <jing07161@gmail.com>
2026-07-25 18:25:24 +0900

    section5>5번 다시 풀기

--- section5/5.공주구하기(큐).py
+++ section5/5.공주구하기(큐).py
@@ -3,17 +3,57 @@ sys.stdin=open('input.txt', 'rt')
 from collections import deque
 
 n,k=map(int, input().split())
-print(n,k)
 
-arr=list(range(1, n+1))
+arr=[i for i in range(1, n+1)]
 
-queue=deque(arr)
-print(queue)
 
-while len(queue)>1:
-  for _ in range(k-1):
-    queue.append(queue.popleft())
-  queue.popleft()
+dq=deque(arr)
+print(dq)
 
-print(queue[0])
+while len(dq)>1:
+  cnt=1
+  while cnt<3:
+    dq.rotate(-1)
+    cnt+=1
+  dq.popleft()
+print(dq)
+arr=list(dq)
+print(arr[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n,k=map(int, input().split())
+# print(n,k)
+
+# arr=list(range(1, n+1))
+
+# queue=deque(arr)
+# print(queue)
+
+# while len(queue)>1:
+#   for _ in range(k-1):
+#     queue.append(queue.popleft())
+#   queue.popleft()
+
+# print(queue[0])
 
