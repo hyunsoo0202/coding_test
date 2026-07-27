4ccd231cd4aaa7bb14d58084731112fde7803c0e
Hyunsoo <jing07161@gmail.com>
2026-07-27 15:37:48 +0900

    section5>6번 다시 풀기

--- section5/6.응급실.py
+++ section5/6.응급실.py
@@ -4,24 +4,85 @@ sys.stdin=open('input.txt', 'rt')
 from collections import deque
 
 n,m=map(int, input().split())
-arr=[(x, idx) for idx, x in enumerate(list(map(int, input().split())))]
-# print(arr)
-queue=deque(arr)
-# print(queue)
+arr=list(map(int, input().split()))
+dq=deque([(idx, x) for (idx,x) in enumerate(arr)])
+print(dq)
 
 cnt=0
 
-
 while True:
-  # print(queue)
-  x=queue.popleft()  
+  target=dq.popleft()
 
-  if any(x[0]<i[0] for i in queue):
-    queue.append(x)
+  if any(target[1]<i[1] for i in dq):
+    dq.append(target)
   else:
     cnt+=1
-    if x[1]==m:
+    if target[0]==m:
       break
+print(cnt)
+
+
+# while True:
+#   target=dq[0]
+#   for i in range(1, len(dq)):
+#     if target[1]<dq[i][1]:
+#       dq.rotate(-1)
+#       break
+#   else:
+#     res=dq.popleft()
+#     cnt+=1
+#     if (res[0]==m):
+#       break
+# print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n,m=map(int, input().split())
+# arr=[(x, idx) for idx, x in enumerate(list(map(int, input().split())))]
+# # print(arr)
+# queue=deque(arr)
+# # print(queue)
+
+# cnt=0
+
+
+# while True:
+#   # print(queue)
+#   x=queue.popleft()  
+
+#   if any(x[0]<i[0] for i in queue):
+#     queue.append(x)
+#   else:
+#     cnt+=1
+#     if x[1]==m:
+#       break
 
   # for y in queue:
     
@@ -33,7 +94,7 @@ while True:
   #   if x[1]==m:
   #     break
       
-print(cnt)
+# print(cnt)
 
 
 
