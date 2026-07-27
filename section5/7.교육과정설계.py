6c14b0e97a80f8b4d13a0c99fb45a26a647e2bc0
Hyunsoo <jing07161@gmail.com>
2026-07-27 16:07:57 +0900

    section5>7번 다시 풀기

--- section5/7.교육과정설계.py
+++ section5/7.교육과정설계.py
@@ -1,25 +1,78 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 from collections import deque
-queue=deque([1,2,3,4,5])
 
-target=input()
+st=input()
 n=int(input())
 
-for _ in range(n):
-  arr=input()
-  queue=deque(target)
-  print(queue)
-  res='YES'
-  cnt=''
-  for x in arr:
-    if x in queue:
-      cnt=queue.popleft()
-      if x!=cnt:
-        res='NO'
+for i in range(n):
+  dq=deque(st)
+  target=input()
+
+  for x in target:
+    if x in dq:
+      val=dq.popleft()
+      if x!=val:
+        print(f"#{i+1} NO")
         break
   else:
-    if len(queue)!=0:
-      res='NO'
-  print(res)
+    if len(dq)!=0:
+      print(f"#{i+1} NO")
+    else:
+      print(f"#{i+1} YES")
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# queue=deque([1,2,3,4,5])
+
+# target=input()
+# n=int(input())
+
+# for _ in range(n):
+#   arr=input()
+#   queue=deque(target)
+#   print(queue)
+#   res='YES'
+#   cnt=''
+#   for x in arr:
+#     if x in queue:
+#       cnt=queue.popleft()
+#       if x!=cnt:
+#         res='NO'
+#         break
+#   else:
+#     if len(queue)!=0:
+#       res='NO'
+#   print(res)
   
