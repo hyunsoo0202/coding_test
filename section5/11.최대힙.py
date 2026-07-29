ede564cf15ba6127d650d3072100dcdac3fe6925
Hyunsoo <jing07161@gmail.com>
2026-07-29 16:26:47 +0900

    section5>11번 다시 풀기

--- section5/11.최대힙.py
+++ section5/11.최대힙.py
@@ -1,17 +1,53 @@
 import sys
 sys.stdin=open('input.txt', 'rt')
 import heapq as hq
-arr=[]
 
+heap=[]
 while True:
-  val=int(input())
-  if val==-1:
+  n=int(input())
+  if n==-1:
     break
-  if val==0:
-    if len(arr)==0:
+  elif n==0:
+    if len(heap)==0:
       print(-1)
     else:
-      print(-hq.heappop(arr))
+      print(-hq.heappop(heap))
   else:
-    hq.heappush(arr, -val)
+    hq.heappush(heap, -n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arr=[]
+
+# while True:
+#   val=int(input())
+#   if val==-1:
+#     break
+#   if val==0:
+#     if len(arr)==0:
+#       print(-1)
+#     else:
+#       print(-hq.heappop(arr))
+#   else:
+#     hq.heappush(arr, -val)
 
