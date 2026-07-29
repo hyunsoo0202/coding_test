23994bcc7a85d6380344239f9eee40fffbb18138
Hyunsoo <jing07161@gmail.com>
2026-07-29 16:24:12 +0900

    section5>10번 다시 풀기

--- section5/10.최소힙.py
+++ section5/10.최소힙.py
@@ -2,17 +2,62 @@ import heapq as hq
 import sys
 sys.stdin=open('input.txt', 'rt')
 
-arr=[]
-
+heap=[]
 while True:
-  val=int(input())
-  if val==-1:
+  n=int(input())
+  if n==-1:
     break
-  elif val==0:
-    if len(arr)==0:
+  elif n==0:
+    if len(hq)==0:
       print(-1)
     else:
-      print(hq.heappop(arr))
-  else:  
-    hq.heappush(arr, val)
+      print(hq.heappop(heap))
+  else: 
+    hq.heappush(heap, n)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arr=[]
+
+# while True:
+#   val=int(input())
+#   if val==-1:
+#     break
+#   elif val==0:
+#     if len(arr)==0:
+#       print(-1)
+#     else:
+#       print(hq.heappop(arr))
+#   else:  
+#     hq.heappush(arr, val)
   
